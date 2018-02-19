567aab2eecb1a43bc295f7c0e8602c3aac2ef5f8
Leah A <32004126+leahnallen@users.noreply.github.com>
2018-02-19 09:11:48 -0800

    Update Character_Houses.sql
    correction

--- Character_Houses.sql
+++ Character_Houses.sql
@@ -1,3 +1,3 @@
-SELECT "FirstName", "LastName", "HouseName", FirstName||" "|| AS FullName
+SELECT "FirstName", "LastName", "HouseName", "FirstName||" "|| AS FullName
 FROM [Characters] JOIN Houses
 ON [Characters].Id = [Houses].Id
